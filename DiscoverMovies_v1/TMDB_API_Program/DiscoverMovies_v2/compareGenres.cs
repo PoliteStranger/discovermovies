1dbe1de3ae15d64a3f7d3ecb4272180cd9bd54d2
Sylvaritius <mads912d@gmail.com>
2022-03-26 19:20:38 +0100

    Functionality started on genre compare
    Mangler korrekt træk af genrer, og mangler den endelige sortering.

--- DiscoverMovies_v1/TMDB_API_Program/DiscoverMovies_v2/compareGenres.cs
+++ DiscoverMovies_v1/TMDB_API_Program/DiscoverMovies_v2/compareGenres.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Razor.Generator;
 using API_DiscoverAlgorithm;
 
 namespace DiscoverMovies_v2
@@ -24,12 +25,58 @@ namespace DiscoverMovies_v2
 
         public List<MovieTitles> SortMethod(List<MovieTitles> movieList)
         {
+            List<Movie> SortedList = new List<Movie>();
+            List<GenreCounter> genreCounterList = new List<GenreCounter>();
+
+
             foreach (var movie in movieList)
             {
-                movie.
+                GenreCompareList genreList = new GenreCompareList();
+                // Henter overordnede film detaljer fra TMDB
+                Task<string> apiResult = ApiOps.RunApiMovieId(movie._id);
+                // Læg resultatet over i en string (json)
+                string jsonMovie = apiResult.Result;
+
+                genreList.ExtractFromJson(jsonMovie);
+
+                foreach (var genre in genreList.genresCompare)
+                {
+                    if (!genreCounterList.Contains(new GenreCounter(genre.GenreId))) //Skal eftercheckes
+                    {
+                        genreCounterList.Add(new GenreCounter(genre.GenreId));
+                        genreCounterList.Find(g => g.GetGenreId() == genre.GenreId).AddCount();//Mulig exception men burde ikke være der da der er tjekket ovenfor
+                    }
+                    else
+                    {
+                        genreCounterList.Find(g => g.GetGenreId() == genre.GenreId).AddCount(); //Mulig exception men burde ikke være der da der er tjekket ovenfor
+                    }
+                }
+            }
+
+            //ikke nødvendig?
+            genreCounterList.Sort(delegate (GenreCounter x, GenreCounter y) //sorterer satanen
+            {
+                return x.GetCount().CompareTo(y.GetCount());
+            });
+
+            List<MovieGenreScore> genreScores = new List<MovieGenreScore>();
+
+            foreach (var movie in movieList)
+            {
+                genreScores.Add(new MovieGenreScore(movie._id));
+            }
+
+            foreach (var movie in genreScores)
+            {
+                
+
 
 
             }
+
+
+
+
 
 
             return movieList;
@@ -43,10 +90,57 @@ namespace DiscoverMovies_v2
 
         // outputter vi den nye liste:
 
+        
+    }
 
+    public class GenreCounter
+    {
+        private int _genreId;
+        private int _count;
 
+        public GenreCounter(int id)
+        {
+            _genreId = id;
+            _count = 0;
+        }
 
+        public void AddCount()
+        {
+            _count++;
+        }
 
+        public int GetCount()
+        {
+            return _count;
+        }
+
+        public int GetGenreId()
+        {
+            return _genreId;
+        }
+    }
+
+    public class MovieGenreScore
+    {
+        private int _movieId { get; }
+        private int _score;
+
+        public MovieGenreScore(int movieId)
+        {
+            _movieId = movieId;
+            _score = 0;
+        }
+
+        public int GetScore()
+        {
+            return _score;
+        }
+
+        public void AddToScore(int val)
+        {
+            _score += val;
+        }
 
     }
+
 }
