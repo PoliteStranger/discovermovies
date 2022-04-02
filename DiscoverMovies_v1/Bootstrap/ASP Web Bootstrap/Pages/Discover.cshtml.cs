70f5ad0574f79a62b5a422602c2ce513b0ccd7ef
Andreas Steen Simmelkiær Sørensen <au196631@uni.au.dk>
2022-04-02 15:51:29 +0200

    Bootstrap projekt tilføjet, og den henter film på forsiden test, fra db.

---
+++ DiscoverMovies_v1/Bootstrap/ASP Web Bootstrap/Pages/Discover.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_Web_Bootstrap.Pages
+{
+    public class DiscoverModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public DiscoverModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
