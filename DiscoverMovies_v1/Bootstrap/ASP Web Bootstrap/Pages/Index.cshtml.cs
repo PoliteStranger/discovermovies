ddf121bb93b4233d1d8db74fe0962744d0626044
patrick8975 <64925851+patrick8975@users.noreply.github.com>
2022-04-04 22:18:31 +0200

    det virker
    det virker

--- DiscoverMovies_v1/Bootstrap/ASP Web Bootstrap/Pages/Index.cshtml.cs
+++ DiscoverMovies_v1/Bootstrap/ASP Web Bootstrap/Pages/Index.cshtml.cs
@@ -15,8 +15,8 @@ namespace ASP_Web_Bootstrap.Pages
 
         // Listen over film som skal vises på en enkelt side:
         private List<Movie> movieList = new List<Movie>();
-        
-        private List<Movie> tings = new List<Movie>();
+        private List<Movie> templiste = new List<Movie>();
+        private List<Genres> tempGenresList = new List<Genres>();
 
 
         // Den bindes, så vi kan tilgå den inde fra vores Razor page
@@ -37,16 +37,10 @@ namespace ASP_Web_Bootstrap.Pages
         // Når vi besøger forsiden får vi følgende:
         public void OnGet()
         {
-
             // Skaber og bruger vores database objekt:
             using (var db = new MyDbContext())
             {
                 TheOriginaleGenres = db.Genres.ToList();
-                foreach (var item in TheOriginaleGenres)
-                {
-                    Console.WriteLine(item._Genrename);
-                }
-                Console.WriteLine("{0}", TheOriginaleGenres.Count);
 
                 // Jeg har en counter på, da jeg ikke vil hente ALLE film (1500+!!!) + mere !!!!!!!
                 int i = 0;
@@ -58,34 +52,83 @@ namespace ASP_Web_Bootstrap.Pages
                     i++;
                     // Når den har hentet 100 film'ish, så stopper vi!
                     if (i == 101) break;
-
                 }
             }
         }
 
         public IActionResult OnPost()
         {
+            using (var db = new MyDbContext())
+            {
+                //dropdown menu skal sættes til at være genrelisten
+                TheOriginaleGenres = db.Genres.ToList();
+
+                if(theinput.Name != "")
+                {
+                    templiste = db.Movies.Where(i => i._title.Contains(theinput.Name)).ToList();
+                    MovieList = templiste;
+                }
+                
+                //den tjekkes om den er sat.
+                if (theinput.GenreID != "0") 
+                {
+                    //cast theinput.GenreID til int
+                    int genreid = Int32.Parse(theinput.GenreID);
+
+                    if (templiste.Count >= 1)
+                    {
+                        //.ToList sikrer, at vi caster til en ny liste.
+                        foreach (var item in templiste.ToList())
+                        {
+                            var tjek = db.GenresAndMovies
+                                .Any(i => i._movieId == item.movieId && i._genreId == genreid);
+                            
+                            if (tjek == false)
+                            {
+                                templiste.Remove(item);
+                            }
+                        }
+                        MovieList = templiste;
+                    }
+
+                    else
+                    {
+                        var theMoviesByGenres = db.GenresAndMovies.Where(i => i._genreId == genreid).ToList();
+                    //hver film i theMoviesByGenres listen bliver slået op i movie db
+                    //film på begge lister bliver tilføjet til temp listen som sættes til Movielisten
+                    foreach (var item in theMoviesByGenres)
+                    {
+                        var film = db.Movies.Find(item._movieId);
+                        templiste.Add(film);
+                    }
+                    MovieList = templiste;
+                    }
+                }
+            }
+            return Page();
+        }
+
+
+        // JUST IN CASE !
+        /*
+        public IActionResult OnPost()
+        {
             Console.WriteLine("Itemname: {0}", theinput.Name);
-            Console.WriteLine(theinput.Genrestring);
+            Console.WriteLine("{0}",theinput.GenreID);
 
             using (var db = new MyDbContext())
             {
-                Console.WriteLine(theinput.Genrestring);
-
-
                 if (theinput.IsMovie == true)
                 {
-                    Console.WriteLine(theinput.Genrestring);
-
                     MovieList = db.Movies.Where(i => i._title.Contains(theinput.Name)).ToList();
 
                     foreach (var item in tings)
                     {
                         Console.WriteLine(tings.Count);
-                        Console.WriteLine(theinput.Genrestring);
-
+                        Console.WriteLine(theinput.GenreID);
                     }
                 }
+
                 if (theinput.IsGenre == true)
                 {
                     var thegenrelist = db.Genres.Where(i => i._Genrename == theinput.Name).ToList();
@@ -108,6 +151,7 @@ namespace ASP_Web_Bootstrap.Pages
             }
             return Page();
         }
+        */
 
         public class InputMovie
         {
@@ -116,11 +160,10 @@ namespace ASP_Web_Bootstrap.Pages
             public string Name { get; set; } = "";
 
             [StringLength(100, ErrorMessage = "Maximum length is {1}")]
-            public string Genrestring { get; set; } = "";
-            
+            public string GenreID { get; set; } = "0";
 
-            public bool IsMovie { get; set; }
-            public bool IsGenre { get; set; }
+            //public bool IsMovie { get; set; }
+            //public bool IsGenre { get; set; }
 
         }
     }
