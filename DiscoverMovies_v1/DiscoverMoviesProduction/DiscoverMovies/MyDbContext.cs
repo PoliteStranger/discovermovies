0432473f6d83472c38d53df9662f0b16e13cfef1
Andreas Steen Simmelkiær Sørensen <au196631@uni.au.dk>
2022-05-03 12:31:24 +0200

    Rettet fejl efter merge

--- DiscoverMovies_v1/DiscoverMoviesProduction/DiscoverMovies/MyDbContext.cs
+++ DiscoverMovies_v1/DiscoverMoviesProduction/DiscoverMovies/MyDbContext.cs
@@ -14,15 +14,13 @@ namespace DiscoverMoviesProduction
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Person> Persons { get; set; }
         public DbSet<Genres> Genres { get; set; }
-<<<<<<<< HEAD:DiscoverMovies_v1/Bootstrap/ASP Web Bootstrap/MyDbContext.cs
+
         public DbSet<ProdCompany> ProdCompany { get; set; }
         public DbSet<Employment> Employments { get; set; }   
-
-========
         public DbSet<Genre> GenresAndMovies { get; set; }
         public DbSet<ProdCompany> ProdCompanies { get; set; }
-        public DbSet<Employment> Employments { get; set; }   
->>>>>>>> PatricksBranch:DiscoverMovies_v1/DiscoverMoviesProduction/DiscoverMovies/MyDbContext.cs
+
+        public DbSet<ProducedBy> ProducedBy { get; set; }
 
     }
 
