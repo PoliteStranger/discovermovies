ece61e5edba8a3afa9c250a2da617f40c339d1eb
PoliteStranger <79262675+DeodorantActivist@users.noreply.github.com>
2022-05-09 21:21:34 +0200

    Added search field - next step persist chosen movies via url

--- DiscoverMovies_v1/DiscoverMoviesProduction/DiscoverMovies/Pages/Discover.cshtml.cs
+++ DiscoverMovies_v1/DiscoverMoviesProduction/DiscoverMovies/Pages/Discover.cshtml.cs
@@ -8,8 +8,21 @@ namespace ASP_Web_Bootstrap.Pages
 {
     public class DiscoverModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public IndexModel.InputMovie theinput { get; set; } = new IndexModel.InputMovie();
 
+        // Listen over film som skal vises på en enkelt side:
+        private List<Movie> movieList = new List<Movie>();
+
+        // Den bindes, så vi kan tilgå den inde fra vores Razor page
+        [BindProperty]
+        public List<Movie> MovieList
+        {
+            get { return movieList; }
+            set { movieList = value; }
+        }
+
+        private readonly ILogger<IndexModel> _logger;
         public DiscoverModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -17,24 +30,50 @@ namespace ASP_Web_Bootstrap.Pages
 
         public void OnGet()
         {
-            List<Movie> inputMovies = new List<Movie>();
-            
-            Discover discover = new Discover();
 
+            //            List<Movie> inputMovies = new List<Movie>();
+            //            
+            //            Discover discover = new Discover();
+            //
+            //            using (var db = new MyDbContext())
+            //            {
+            //                // 5 random film som er i db!
+            //                // 8374, 1542, 603, 564, 3293
+            //                // Til 27205, 329, 553, 271110, 862
+            //                inputMovies.Add(db.Movies.Where(c => c.movieId == 11398).Include(x => x._genreList).Include(y => y._prodCompanyList).Include(z => z._employmentList).FirstOrDefault());
+            //                inputMovies.Add(db.Movies.Where(c => c.movieId == 955).Include(x => x._genreList).Include(y => y._prodCompanyList).Include(z => z._employmentList).FirstOrDefault());
+            //                inputMovies.Add(db.Movies.Where(c => c.movieId == 180).Include(x => x._genreList).Include(y => y._prodCompanyList).Include(z => z._employmentList).FirstOrDefault());
+            //                inputMovies.Add(db.Movies.Where(c => c.movieId == 2787).Include(x => x._genreList).Include(y => y._prodCompanyList).Include(z => z._employmentList).FirstOrDefault());
+            //                inputMovies.Add(db.Movies.Where(c => c.movieId == 107).Include(x => x._genreList).Include(y => y._prodCompanyList).Include(z => z._employmentList).FirstOrDefault());
+            //            }
+            //
+            //
+            //            discover.DiscoverMovies(inputMovies);
+        }
+
+        public IActionResult OnPost()
+        {
             using (var db = new MyDbContext())
             {
-                // 5 random film som er i db!
-                // 8374, 1542, 603, 564, 3293
-                // Til 27205, 329, 553, 271110, 862
-                inputMovies.Add(db.Movies.Where(c => c.movieId == 11398).Include(x => x._genreList).Include(y => y._prodCompanyList).Include(z => z._employmentList).FirstOrDefault());
-                inputMovies.Add(db.Movies.Where(c => c.movieId == 955).Include(x => x._genreList).Include(y => y._prodCompanyList).Include(z => z._employmentList).FirstOrDefault());
-                inputMovies.Add(db.Movies.Where(c => c.movieId == 180).Include(x => x._genreList).Include(y => y._prodCompanyList).Include(z => z._employmentList).FirstOrDefault());
-                inputMovies.Add(db.Movies.Where(c => c.movieId == 2787).Include(x => x._genreList).Include(y => y._prodCompanyList).Include(z => z._employmentList).FirstOrDefault());
-                inputMovies.Add(db.Movies.Where(c => c.movieId == 107).Include(x => x._genreList).Include(y => y._prodCompanyList).Include(z => z._employmentList).FirstOrDefault());
+                if (theinput.Name != "")
+                {
+                    var movieSearchResult = db.Movies.FirstOrDefault(i => i._title == theinput.Name);
+                    if (movieSearchResult != null)
+                    {
+                        MovieList.Add(movieSearchResult);
+                    }
+                }
+
+//                var theMovies = db.Movies.ToList();
+//                //hver film i theMoviesByGenres listen bliver slået op i movie db
+//                //film på begge lister bliver tilføjet til temp listen som sættes til Movielisten
+//                foreach (var item in theMovies)
+//                {
+//                    templiste.Add(item);
+//                }
+
+                return Page();
             }
-
-
-            discover.DiscoverMovies(inputMovies);
         }
     }
 }
